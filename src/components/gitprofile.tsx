57ced8e2d4fbfe47e1effa1a6f6531288c66e0a1
Gabriel C. Rodríguez <gabrieljcr19@gmail.com>
2025-07-19 18:33:34 -0400

    Update gitprofile.tsx

--- src/components/gitprofile.tsx
+++ src/components/gitprofile.tsx
@@ -242,13 +242,10 @@ const GitProfile = ({ config }: { config: Config }) => {
             {/* --- RIGHT COLUMN --- */}
             <div className="lg:col-span-2 col-span-1">
               <div className="grid grid-cols-1 gap-6">
-                {sanitizedConfig.projects.github.display && (
-                  <GithubProjectCard
-                    header={sanitizedConfig.projects.github.header}
-                    limit={sanitizedConfig.projects.github.automatic.limit}
-                    githubProjects={githubProjects}
+               {sanitizedConfig.publications.length !== 0 && (
+                  <PublicationCard
                     loading={loading}
-                    googleAnalyticsId={sanitizedConfig.googleAnalytics.id}
+                    publications={sanitizedConfig.publications}
                   />
                 )}
                 {sanitizedConfig.projects.external.projects.length !== 0 && (
@@ -259,10 +256,13 @@ const GitProfile = ({ config }: { config: Config }) => {
                     googleAnalyticId={sanitizedConfig.googleAnalytics.id}
                   />
                 )}
-                {sanitizedConfig.publications.length !== 0 && (
-                  <PublicationCard
+                {sanitizedConfig.projects.github.display && (
+                  <GithubProjectCard
+                    header={sanitizedConfig.projects.github.header}
+                    limit={sanitizedConfig.projects.github.automatic.limit}
+                    githubProjects={githubProjects}
                     loading={loading}
-                    publications={sanitizedConfig.publications}
+                    googleAnalyticsId={sanitizedConfig.googleAnalytics.id}
                   />
                 )}
                 {sanitizedConfig.blog.display && (
