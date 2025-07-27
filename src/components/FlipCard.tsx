cc0fbfa9c4a5524ed0754be28a17d169ea691bff
Gabriel C. Rodríguez <gabrieljcr19@gmail.com>
2025-07-27 11:36:46 -0400

    Update FlipCard.tsx

--- src/components/FlipCard.tsx
+++ src/components/FlipCard.tsx
@@ -1,8 +1,7 @@
 // FlipCard.tsx
 import React from 'react';
-
-const FlipCard = ({ front, back }: { front: React.ReactNode; back: React.ReactNode }) => (
-  <div className="group w-full h-full perspective">
+const FlipCard = ({ front, back }) => (
+  <div className="group w-full h-full perspective min-h-[400px]"> {/* fix here */}
     <div className="relative w-full h-full transition-transform duration-700 preserve-3d group-hover:rotate-y-180">
       <div className="absolute inset-0 backface-hidden">
         {front}
